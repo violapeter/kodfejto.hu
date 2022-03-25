c380203b81a02d1f74e7771e9d3541f1d3de887d
Péter Viola <info@violapeter.hu>
2022-03-25 11:10:19 +0100

    style: fix eslint

--- src/hooks/useDragImage.ts
+++ src/hooks/useDragImage.ts
@@ -1,4 +1,10 @@
-import { MutableRefObject, useCallback, useEffect, useRef } from 'react'
+import {
+  MutableRefObject,
+  useCallback,
+  useEffect,
+  useRef,
+  useState,
+} from 'react'
 
 type UseDragImage = <T extends HTMLElement>() => {
   ref: MutableRefObject<T | null>
@@ -6,22 +12,23 @@ type UseDragImage = <T extends HTMLElement>() => {
 
 export const useDragImage: UseDragImage = <T extends HTMLElement>() => {
   const ref = useRef<T>(null)
-  let clone: T
+  const [clone, setClone] = useState<T>()
 
   const handleDragStart = useCallback((event) => {
     const target = event.target as T
-    clone = target.cloneNode() as T
-    clone.style.setProperty('cursor', 'grabbing')
-    clone.style.setProperty('position', 'absolute')
-    clone.style.setProperty('top', '-1000px')
+    const cloneEl = target.cloneNode() as T
+    setClone(cloneEl)
+    cloneEl.style.setProperty('cursor', 'grabbing')
+    cloneEl.style.setProperty('position', 'absolute')
+    cloneEl.style.setProperty('top', '-1000px')
     const offset = target.offsetHeight / 2
-    event.dataTransfer?.setDragImage(clone, offset, offset)
-    document.body.appendChild(clone)
+    event.dataTransfer?.setDragImage(cloneEl, offset, offset)
+    document.body.appendChild(cloneEl)
   }, [])
 
   const handleDragEnd = useCallback(() => {
-    clone.remove()
-  }, [])
+    clone?.remove()
+  }, [clone])
 
   useEffect(() => {
     if (ref.current === null || ref.current === undefined) {
