f952f29f02c82ffff67a05741db9467164de8854
Péter Viola <info@violapeter.hu>
2022-03-25 11:01:49 +0100

    fix: rewrite useDragImage hook

--- src/hooks/useDragImage.ts
+++ src/hooks/useDragImage.ts
@@ -1,4 +1,4 @@
-import { MutableRefObject, useEffect, useRef } from 'react'
+import { MutableRefObject, useCallback, useEffect, useRef } from 'react'
 
 type UseDragImage = <T extends HTMLElement>() => {
   ref: MutableRefObject<T | null>
@@ -6,25 +6,38 @@ type UseDragImage = <T extends HTMLElement>() => {
 
 export const useDragImage: UseDragImage = <T extends HTMLElement>() => {
   const ref = useRef<T>(null)
+  let clone: T
+
+  const handleDragStart = useCallback((event) => {
+    const target = event.target as T
+    clone = target.cloneNode() as T
+    clone.style.setProperty('cursor', 'grabbing')
+    clone.style.setProperty('position', 'absolute')
+    clone.style.setProperty('top', '-1000px')
+    const offset = target.offsetHeight / 2
+    event.dataTransfer?.setDragImage(clone, offset, offset)
+    document.body.appendChild(clone)
+  }, [])
+
+  const handleDragEnd = useCallback(() => {
+    clone.remove()
+  }, [])
 
   useEffect(() => {
-    let clone: T
     if (ref.current === null || ref.current === undefined) {
       return
     }
 
     const { current: el } = ref
 
-    el.addEventListener('dragstart', (event) => {
-      const target = event.target as T
-      clone = el.cloneNode() as T
-      clone.style.setProperty('cursor', 'grabbing')
-      const offset = target.offsetHeight / 2
-      event.dataTransfer?.setDragImage(clone, offset, offset)
-      document.body.appendChild(clone)
-    })
-    el.addEventListener('dragend', () => clone.remove())
-  }, [ref])
+    el.addEventListener('dragstart', handleDragStart)
+    el.addEventListener('dragend', handleDragEnd)
+
+    return () => {
+      el.addEventListener('dragstart', handleDragStart)
+      el.addEventListener('dragend', handleDragEnd)
+    }
+  }, [ref, handleDragStart, handleDragEnd])
 
   return {
     ref,
